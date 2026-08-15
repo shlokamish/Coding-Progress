d0f95ea6cf591a6d2b34d3db4c2372904be8796b
Shloka Mishra <shlokamishra08@gmail.com>
2026-08-15 12:21:50 +0530

    day3

---
+++ day3.java
@@ -0,0 +1,33 @@
+//STRINGS
+/*import java.util.Scanner;
+class main{
+    public static void main (String[] args){
+        String name="Peter";
+        System.out.println(name);
+        Scanner s= new Scanner(System.in);
+        String petname=s.next();
+        s.nextLine(); // consume the leftover Enter
+        // next() leaves \n behind, so nextLine() reads it.
+        String fullname=s.nextLine();
+        System.out.println(petname +" "+ fullname);
+        System.out.println(name.length()); // STRING LENGTH
+        System.out.println(name.charAt(0));
+        System.out.println(petname.charAt(2));//ACCESSING A CHARCTER
+        if(name.equals("Peter")) {
+            System.out.println("Correct"); //STRING COMPARISON
+        }          
+    }
+}*/
+
+
+/*STRING METHODS
+1. name.length() //number of characters
+2. name.toUpperCase() //uppercase all characters
+3. name.toLowerCase() // lowercase all characters
+4. name.charAt(0) //access character at a index
+5. name.contains("Peter") //checks if something exists
+6. name.equals("Peter Parker") //compares two Strings
+7. name.substring(6) //Used to take a part of a String. RESULT: Parker (From that string to end)
+8. name.substring(0, 5) //Start index is included, end index is NOT included.
+*/
+
